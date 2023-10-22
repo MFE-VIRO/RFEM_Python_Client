ba835dbc51ae50f577d876314e6855306c131410
MFE-VIRO <m.francke@viro-group.com>
2023-10-22 13:39:50 +0200

    Cleanup

---
+++ MFE_getMembers.py
@@ -0,0 +1,13 @@
+
+from RFEM.enums import ObjectTypes
+from RFEM.initModel import Model
+from RFEM.BasicObjects.member import Member
+from RFEM.Tools.GetObjectNumbersByType import GetObjectNumbersByType
+
+def getMembers(model = Model):
+    MemberNumbers = GetObjectNumbersByType(ObjectTypes.E_OBJECT_TYPE_MEMBER,model)
+    members = []
+    for n in MemberNumbers:
+        member = Member.GetMember(n,model)
+        members.append({"no":member.no,"startNode": member.node_start, "endNode":member.node_end})
+    return members
