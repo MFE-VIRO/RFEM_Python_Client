043452c20cbc5a770a25e232f681d33ae3fe2faa
MFE-VIRO <m.francke@viro-group.com>
2023-11-01 20:40:56 +0100

    adffdas

--- MFE_Wind_Walls_and_Flat_Roof.py
+++ MFE_Wind_Walls_and_Flat_Roof.py
@@ -23,9 +23,29 @@ def Wind(
     e=[min(b[0],2*h),min(b[1],2*h)]
 
     Model.clientModel.service.begin_modification()
-    FreeLoad.PolygonLoad(FirstFreeIdNumber(ObjectTypes.E_OBJECT_TYPE_FREE_POLYGON_LOAD,model=model),load_case_no=401,surfaces_no=1)
 
     BaseNode = Node.GetNode(BaseNodeNo,model)
+    xb = BaseNode.coordinate_1
+    yb = BaseNode.coordinate_2
+    zb = BaseNode.coordinate_3
+    # FreeLoad.PolygonLoad(FirstFreeIdNumber(ObjectTypes.E_OBJECT_TYPE_FREE_POLYGON_LOAD,0,model),
+    #                      load_case_no=401,surfaces_no=1,
+    #                      load_projection=FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
+    #                      load_direction=FreePolygonLoadLoadDirection.LOAD_DIRECTION_LOCAL_Z,
+    #                      load_location={'free_polygon_load_load_location': [{'no': 1,'row': {'first_coordinate': xb, 'second_coordinate': yb+b[0],}},
+    #                      {'no': 2, 'row': {'first_coordinate': xb+3, 'second_coordinate': yb+b[0]}},
+    #                      {'no': 3, 'row': {'first_coordinate': xb+3, 'second_coordinate': yb+b[0]-6}},
+    #                      {'no': 4, 'row': {'first_coordinate': xb, 'second_coordinate': yb+b[0]-6}}]},params={'magnitude_uniform': -5000.0})
+
+    FreeLoad.PolygonLoad(0,
+                         load_case_no=401,surfaces_no=1,
+                         load_projection=FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
+                         load_direction=FreePolygonLoadLoadDirection.LOAD_DIRECTION_LOCAL_Z,
+                         load_location={'free_polygon_load_load_location': [{'no': 1,'row': {'first_coordinate': xb, 'second_coordinate': yb+b[0],}},
+                         {'no': 2, 'row': {'first_coordinate': xb+3, 'second_coordinate': yb+b[0]}},
+                         {'no': 3, 'row': {'first_coordinate': xb+3, 'second_coordinate': yb+b[0]-6}},
+                         {'no': 4, 'row': {'first_coordinate': xb, 'second_coordinate': yb+b[0]-6}}]},params={'magnitude_uniform': -5000.0})
+
     Model.clientModel.service.finish_modification()
 
 if __name__ == '__main__':
