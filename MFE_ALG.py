fc71ea9d25d9009ecea291fb7e7d8f7b28c2e165
MFE-VIRO <m.francke@viro-group.com>
2023-11-13 20:30:58 +0100

    vzxcv

--- MFE_ALG.py
+++ MFE_ALG.py
@@ -1,6 +1,12 @@
+import math
+
 def interpol(x=1.0,x1 = 0.0, x2=2.0,y1=5.0,y2=10.0):
     y = y1 + (x - x1) / (x2 - x1) * (y2 - y1)
     return y
+
+# def logInterpol(x=1.0,x1 = 0.0, x2=2.0,y1=5.0,y2=10.0):
+#     y = (y2-y1) / (math.log(x2)-math.log(x1)) * (math.log(x)-math.log(x1))
+#     return y
 
 def left(s, amount):
     return s[:amount]
---
+++ MFE_QUESTION_MIF_by_Section.py
@@ -0,0 +1,78 @@
+from RFEM.enums import *
+from RFEM.initModel import Model, Calculate_all
+from RFEM.baseSettings import BaseSettings
+from RFEM.BasicObjects.material import Material
+from RFEM.BasicObjects.section import Section
+from RFEM.BasicObjects.node import Node
+from RFEM.BasicObjects.member import Member
+from RFEM.TypesForNodes.nodalSupport import NodalSupport
+from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import StaticAnalysisSettings
+from RFEM.LoadCasesAndCombinations.designSituation import clearAttributes, DesignSituation
+from RFEM.LoadCasesAndCombinations.loadCase import LoadCase
+from RFEM.LoadCasesAndCombinations.loadCombination import LoadCombination
+from RFEM.LoadCasesAndCombinations.loadCasesAndCombinations import LoadCasesAndCombinations
+from RFEM.LoadCasesAndCombinations.combinationWizard import CombinationWizard
+from RFEM.Loads.memberLoad import MemberLoad
+from RFEM.dataTypes import inf
+from RFEM.Results.resultTables import ResultTables, GetMaxValue, GetMinValue
+from RFEM.Results.designOverview import GetDesignOverview, GetPartialDesignOverview
+
+import math
+
+if __name__ == '__main__':
+    Model(True, "ExampleMIFbySection.rf6")
+    Model.clientModel.service.delete_all()
+    Model.clientModel.service.begin_modification()
+    BaseSettings(9.81, GlobalAxesOrientationType.E_GLOBAL_AXES_ORIENTATION_ZUP, LocalAxesOrientationType.E_LOCAL_AXES_ORIENTATION_ZDOWN)
+
+    Material(1, 'S235JR | EN 10025-2:2019-08')
+
+    Section(11, 'IPE 200 | Euronorm 19-57; ... | SZS', 1)
+    Section(12, 'IPE 240 | Euronorm 19-57; ... | SZS', 1)
+
+    Node(1, 0.3, 0.0, 0.0)
+    Node(2, 3.1, 0.0, 0.0)
+    Node(3, 0.1, 2.0, 0.0)
+    Node(4, 3.5, 2.0, 0.0)
+    Node(5, 0.9, 4.0, 0.0)
+    Node(6, 4.2, 4.0, 0.0)
+
+    Member(21, 1, 2, 0, 11, 11)
+    Member(22, 3, 4, 0, 12, 12)
+    Member(22, 5, 6, 0, 12, 12)
+
+    StaticAnalysisSettings.GeometricallyLinear(1, "Linear")
+
+    LoadCasesAndCombinations({
+                    "current_standard_for_combination_wizard": 6047,
+                    "activate_combination_wizard_and_classification": True,
+                    "activate_combination_wizard": True,
+                    "result_combinations_active": False,
+                    "result_combinations_parentheses_active": False,
+                    "result_combinations_consider_sub_results": False,
+                    "combination_name_according_to_action_category": False},
+                 model= Model)
+
+    LoadCase.StaticAnalysis(10, 'DL: Self-weight',analysis_settings_no=1,action_category=ActionCategoryType.ACTION_CATEGORY_PERMANENT_G, self_weight=[True, 0.0, 0.0, -1.0])
+    LoadCase.StaticAnalysis(20, 'DL: Other dead loads',analysis_settings_no=1,action_category=ActionCategoryType.ACTION_CATEGORY_PERMANENT_G, self_weight=[False])
+    LoadCase.StaticAnalysis(100, 'LL: Imposed loads',analysis_settings_no=1,action_category=ActionCategoryType.ACTION_CATEGORY_IMPOSED_LOADS_CATEGORY_B_OFFICE_AREAS_QI_B, self_weight=[False])
+
+    CombinationWizard(1,consider_imperfection_case=False)
+    DesignSituation(1,DesignSituationType.DESIGN_SITUATION_TYPE_STR_PERMANENT_AND_TRANSIENT_6_10A_6_10B,
+                    True,'ULS (STR/GEO) - Permanent and transient - Eq. 6.10a and 6.10b',
+                    params={'combination_wizard': 1})
+
+    MemberLoad(1,5,insertSpaces(TensionMembers),MemberLoadDirection.LOAD_DIRECTION_LOCAL_X,100)
+
+
+
+
+Model.clientModel.service.finish_modification()
+Calculate_all()
+
+
+MIF = ResultTables.MembersInternalForces(CaseObjectType.E_OBJECT_TYPE_DESIGN_SITUATION,1,17)
+MIFS = ResultTables.MembersInternalForcesBySection(CaseObjectType.E_OBJECT_TYPE_DESIGN_SITUATION,1,2,False)
+
+print("test")
+print("T1 = " + str(time.time()-time1) + "s")
