a7d1fe681f7ee23e3dbdcc743e7d9ac1bc57e267
MFE-VIRO <m.francke@viro-group.com>
2023-11-19 15:50:33 +0100

    rqwe

--- MFE_Plaat_met_UNP.py
+++ MFE_Plaat_met_UNP.py
@@ -1,21 +1,22 @@
 import math
 from operator import itemgetter
 
-Lpr = 1400 # = lengte UNP profiel in mm.
-fyPl = 235
-fyPr = 235
-E = 210000
+fyPl = 235 # = vloeispanning plaat in N/mm²
+fyPr = 235 # = vloeispanning profiel in N/mm²
+E = 210000 # = elasticiteitsmodulus in N/mm²
 RhoS = 7850 # soortelijk gewicht staal in kg/m3
+
+Lpr = 1400 # = lengte UNP profiel in mm.
 maxRelUitnPl = 1.00 # = Maximale relatieve uitnutting van de vloeispanning van de plaat. Maximale waarde is 1.00.
 maxRelUitnPr = 1.00 # = Maximale relatieve uitnutting van de vloeispanning van het profiel. Maximale waarde is 1.00.
-uRelMaxPl = 1/500
-uRelMaxPr = 1/500
+uRelMaxPl = 1/500 # = maximaal toelaatbare relatieve doorbuiging plaat [-]
+uRelMaxPr = 1/500 # = maximaal toelaatbare relatieve doorbuiging pprofiel [-]
+RhoWater = 10.5 # = specific weight water in kN/m3
+WaterLevel = 4.85 # = waterkolom op belaste oppervlak in m
 
 p_k = 4.85*10.5 # = hydrostatic waterpressure in kN/m²
 p_Ed = p_k*1.35 # = hydrostatic waterpressure in kN/m²
-
 Results = []
-
 t_list = [12,15,20,25] #plaatdikte t in mm.
 profielen = [{'naam':'UNP80','G8':8.82,'G':8.65,'A':1102,'I_z_el':1060000,'W_z_el':26500},
              {'naam':'UNP100','G8':10.80,'G':10.56,'A':1345,'I_z_el':2050000,'W_z_el':41100},
@@ -35,7 +36,15 @@ for profiel in profielen:
         ctcPrU = (uRelMaxPr*384*E*profiel['I_z_el'])/(5*p_k*0.001*Lpr**3)
         ctc = min(ctcPlM,ctcPlU,ctcPrM,ctcPrU)
         Weight = t/1000*RhoS+profiel['G']*1000/ctc
-        Results.append([profiel['naam'],t,round(ctc),round(Weight,1)])
+        Upl = (p_k*0.001*1000*ctc**4)/(192*E*1/12*1000*t**3)
+        Upr = (5*p_k*0.001*ctc*Lpr**4)/(384*E*profiel['I_z_el'])
+        stressPl = (1/8*p_Ed*0.001*1000*ctc**2)/(1/6*1000*t**2)
+        stressPr = (1/8*p_Ed*0.001*ctc*Lpr**2)/(profiel['W_z_el'])
+        UC_Upl = ctc/ctcPlU
+        UC_Upr = ctc/ctcPrU
+        UC_Mpl = ctc/ctcPlM
+        UC_Mpr = ctc/ctcPrM
+        Results.append([profiel['naam'],t,round(ctc),round(Weight,1),round(Upl,2),round(Upr,2),round(stressPl),round(stressPr),round(UC_Upl,3),round(UC_Upr,3),round(UC_Mpl,3),round(UC_Mpr,3)])
 
 Results = sorted(Results, key=itemgetter(3))
 for i in Results:
